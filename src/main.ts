d01e20a049a48b28177595f497c435886e29a818
vietlinhg4 <vietlinhg7@gmail.com>
2024-11-03 15:19:37 +0700

    change origin for cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // Set to your frontend’s deployed URL
+    origin: 'https://venerable-seahorse-21ac66.netlify.app', // Set to your frontend’s deployed URL
     credentials: true,
   });
   await app.listen(process.env.PORT || 3000);
