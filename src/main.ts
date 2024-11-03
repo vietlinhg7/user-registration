eecbb175623a8a30ec962c863dc239ccb22da218
vietlinhg4 <vietlinhg7@gmail.com>
2024-11-03 14:52:45 +0700

    second commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS for the React frontend
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3001', // Set to your frontend’s deployed URL
+    credentials: true,
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
